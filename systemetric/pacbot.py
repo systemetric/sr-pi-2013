1eeb0a9f27ad99a5b70b32ac0a52e9135b7454f2
Cheng Sun <chengsun9@gmail.com>
2013-10-15 18:33:55 +0100

    Manually add location to libkoki.so

--- systemetric/pacbot.py
+++ systemetric/pacbot.py
@@ -3,7 +3,7 @@ from robot import Robot
 class PacBot(Robot):
     def __init__(self, mode):
         self.mbed = Mbed('/dev/ttyACM0', 115200)
-        self.vision = VisionShim('/dev/video0')
+        self.vision = VisionShim('/dev/video0', '/usr/lib/arm-linux-gnueabihf')
         self.leftMotor = MotorMbed(self.mbed, 0)
         self.rightMotor = MotorMbed(self.mbed, 1)
         self.motorDriver = BlindMotorDriver(self.leftMotor, self.rightMotor,
