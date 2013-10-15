c6fc289f19507d71d016a0aca67a6938827e0398
MountainNinja <Oliver.Howard.2000@gmail.com>
2013-10-15 16:55:48 +0100

    functions for moving and turning added

--- systemetric/pacbot.py
+++ systemetric/pacbot.py
@@ -1,4 +1,16 @@
 from robot import Robot
 
 class PacBot(Robot):
-    pass
+    def __init__(self, mode):
+        self.mbed = Mbed('/dev/ttyACM0', 115200)
+        self.vision = VisionShim('/dev/video0')
+        self.leftMotor = MotorMbed(self.mbed, 0)
+        self.rightMotor = MotorMbed(self.mbed, 1)
+        self.motorDriver = BlindMotorDriver(self.leftMotor, self.rightMotor,
+                                            1, 1, 1, 1) #right, left, dist, turn
+
+        super(LifterBot, self).__init__(mode)
+    def turn(angle):
+        self.motorDriver.turn(angle)
+    def moveForward(distance):
+        self.motorDriver.moveForward(distance)
