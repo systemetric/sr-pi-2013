288d00aba78fffc593455a712cc34630f1dfc510
Cheng Sun <chengsun9@gmail.com>
2013-10-16 12:15:47 +0100

    More fixes

--- systemetric/blindmotordriver.py
+++ systemetric/blindmotordriver.py
@@ -16,11 +16,11 @@ class BlindMotorDriver(object):
         try:
             if distance < 0: #if the distance is negative, it becomes positive        
                 distance = -distance       
-                self.motorR.setPower(100*rightMotorFactor)
-                self.motorL.setPower(100*leftMotorFactor)
+                self.motorR.setPower(100*self.rightMotorFactor)
+                self.motorL.setPower(100*self.leftMotorFactor)
             else:        
-                self.motorR.setPower(-100*rightMotorFactor)
-                self.motorL.setPower(-100*leftMotorFactor)
+                self.motorR.setPower(-100*self.rightMotorFactor)
+                self.motorL.setPower(-100*self.leftMotorFactor)
             time.sleep(distance*self.distanceCalibration)
         finally:
             self.motorR.setPower(0)
@@ -37,8 +37,8 @@ class BlindMotorDriver(object):
             angle = -angle
             direction = -1
         try:
-            self.motorR.setPower(direction*100*rightMotorFactor)    
-            self.motorL.setPower(direction*100*leftMotorFactor)   
+            self.motorR.setPower(direction*100*self.rightMotorFactor)    
+            self.motorL.setPower(direction*100*self.leftMotorFactor)   
             time.sleep(self.turnCalibration*angle/90.)
         finally:
             self.motorR.setPower(0)    
