c9108d7c3127db47bc03b84be1665940d21c37c5
Cheng Sun <chengsun9@gmail.com>
2013-10-16 14:10:29 +0100

    Update calibration

--- systemetric/pacbot.py
+++ systemetric/pacbot.py
@@ -10,7 +10,7 @@ class PacBot(Robot):
         self.leftMotor = MotorMbed(self.mbed, 0)
         self.rightMotor = MotorMbed(self.mbed, 1)
         self.motorDriver = BlindMotorDriver(self.leftMotor, self.rightMotor,
-                                            1, -1, 1, 1) #right, left, dist, turn
+                                            1, -1, 1, 0.5) #right, left, dist, turn
         self.arm = Arm(self.mbed)
         self.pump = Pump(self.mbed)
 
