7da16b3ab07436f0d20ba37293643538752dcc1c
Cheng Sun <chengsun9@gmail.com>
2013-10-17 12:08:40 +0100

    Move power factor into motor rather than driver (1/2)

--- systemetric/blindmotordriver.py
+++ systemetric/blindmotordriver.py
@@ -1,12 +1,9 @@
 import time
 class BlindMotorDriver(object):
-    def __init__(self, leftMotor, rightMotor, 
-                 leftMotorFactor=1, rightMotorFactor=1,
+    def __init__(self, leftMotor, rightMotor,
                  distanceCalibration=1, turnCalibration=1):
         self.motorR = rightMotor
         self.motorL = leftMotor
-        self.rightMotorFactor = rightMotorFactor
-        self.leftMotorFactor = leftMotorFactor
         self.distanceCalibration = distanceCalibration
         self.turnCalibration = turnCalibration
     def moveForward(self, distance):    
@@ -15,12 +12,12 @@ class BlindMotorDriver(object):
         input: distance"""
         try:
             if distance < 0: #if the distance is negative, it becomes positive        
-                distance = -distance       
-                self.motorR.setPower(100*self.rightMotorFactor)
-                self.motorL.setPower(100*self.leftMotorFactor)
+                distance = -distance
+                self.motorR.setPower(100)
+                self.motorL.setPower(100)
             else:        
-                self.motorR.setPower(-100*self.rightMotorFactor)
-                self.motorL.setPower(-100*self.leftMotorFactor)
+                self.motorR.setPower(-100)
+                self.motorL.setPower(-100)
             time.sleep(distance*self.distanceCalibration)
         finally:
             self.motorR.setPower(0)
@@ -37,8 +34,8 @@ class BlindMotorDriver(object):
             angle = -angle
             direction = -1
         try:
-            self.motorR.setPower(direction*100*self.rightMotorFactor)    
-            self.motorL.setPower(-direction*100*self.leftMotorFactor)   
+            self.motorR.setPower(direction*100)
+            self.motorL.setPower(-direction*100)
             time.sleep(self.turnCalibration*angle/90.)
         finally:
             self.motorR.setPower(0)    
